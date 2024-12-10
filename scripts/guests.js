08d50c87cdb520e0ed7534f6658ec4289fe8244a
Brian Henry <brianhenrytech@gmail.com>
2024-12-10 13:39:33 -0600

    Stretch goalsish

--- scripts/guests.js
+++ scripts/guests.js
@@ -3,29 +3,32 @@ import { getGuests, getDestinations } from "./database.js";
 const guests = getGuests();
 const destinations = getDestinations();
 
-const getGuestDestination = (guestLocationId) =>
-    destinations.find(({id: destId})=> destId === parseInt(guestLocationId)).name
+export const getGuestDestination = (guestLocationId) =>
+	destinations.find(({ id: destId }) => destId === parseInt(guestLocationId))
+		.name;
 
-document.addEventListener(
-    "click",
-    (clickTarget) => {
-        if (clickTarget.target.dataset.type === "guest") { 
-            window.alert(`This guest is at the ${getGuestDestination(clickTarget.target.dataset.locationid)}`)
-        }
-    })
+document.addEventListener("click", (clickTarget) => {
+	if (clickTarget.target.dataset.type === "guest") {
+		window.alert(
+			`This guest is at the ${getGuestDestination(clickTarget.target.dataset.locationid)}`,
+		);
+	}
+});
 
 export const genGuestHtml = () => `
         <ul><h2>Guests</h2>
-        ${  
-            guests.map( ( {id, destinationId, name} ) => `
+        ${guests
+					.map(
+						({ id, destinationId, name }) => `
                 <li id="guest-li"
                         data-type="guest"
                         data-locationId="${destinationId}"
                         data-id="${id}">
                         ${name} (${getGuestDestination(destinationId)})
                 </li>
-                `)
-                .join("")
-            }
+                `,
+					)
+					.join("")}
         </ul>
-        `
+        `;
+
